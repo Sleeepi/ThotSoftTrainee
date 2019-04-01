78e9a828b5d62611ef293b088cad4084b0362465
Sleeepi <34219347+Sleeepi@users.noreply.github.com>
2019-04-01 14:23:01 +0200

    Update ReadTest.java

--- src/readertest/ReadTest.java
+++ src/readertest/ReadTest.java
@@ -1,14 +1,40 @@
 package readertest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
-class ReadTest {
+import static org.junit.Assert.assertEquals;
+import static org.junit.runners.Parameterized.*;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+@RunWith(Parameterized.class)
+public class ReadTest {
+
+    // fields used together with @Parameter must be public
+    @Parameter(0)
+    String m1;
+    @Parameter(1)
+    String m2;
+
+
+    // creates the test data
+    @Parameters
+    public static Collection<Object[]> data() {
+        Object[][] data = new Object[][] { { "trak.jpg" ,"C:/Hegga/sample.txt", "" }, { "", "C:/Hegga/copy.txt", "15452" } };
+        return Arrays.asList(data);
+    }
+
+
+    @Test
+    public void ReaderTestException() {
+        ReaderTest tester = new ReaderTest();
+        assertEquals(tester.ReaderMethod(m1, m2));
+    }
+
+
 
 }
