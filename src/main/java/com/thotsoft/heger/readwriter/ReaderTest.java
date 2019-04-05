5d381de72a32a5c5fecc8ec270bcffe98e376030
Sleeepi <34219347+Sleeepi@users.noreply.github.com>
2019-04-05 11:43:23 +0200

    Add files via upload

--- src/main/java/com/thotsoft/heger/readwriter/ReaderTest.java
+++ src/main/java/com/thotsoft/heger/readwriter/ReaderTest.java
@@ -8,45 +8,109 @@ import java.io.IOException;
 import java.util.*;
 
 public class ReaderTest {
-	
+
 
 	public static void main (String[] args) throws Exception {
 		
-		
-	read(args[0],args[1]);
-	
-	//UnderDevelopment
-	/*System.out.println("Write your filename here.");
-	Scanner input = new Scanner(System.in);
-	String filename = input.nextLine();
-	System.out.println("Your filename is" + filename);
-	File myFile = new File (filename);
-	 if (!myFile.exists()) {
-         System.out.println("Not Found");
-         Thread.sleep(2000);
-     }
-	    
-	System.out.println("Would you like to copy the first file data to another file?");
-	System.out.println("Yes: Please write another output.");
-	System.out.println("No. Just press the enter.");
-	Scanner input2 =new Scanner(System.in);
-	String filename2 = input2.nextLine();
-	System.out.println("Your filename is "+ filename2);
-	//String filename = "C:/Hegga/sample.txt";
-	//String filename2 = "";
-	/*ReaderMethod(filename, filename2);
-	
-	ReaderMethod("C:/Hegga/sample.txt", "");
-	ReaderMethod("", "");
-	ReaderMethod("C:/Hegga/sample1.txt", "C:/Hegga/342342.txt");
-	ReaderMethod("C:/Hegga/tara1.txt", ""); */
-		
-
+//args	
+	if (args[0].length()== 0) {
+		System.out.println("Not a parameter.");
 	}
-public static void read(String filename, String filename2) {
+//source
+	if(args[0] != "--source") {
+		System.out.println("Wrong Command!");
+	}else if(!args[1].contains(".txt")) {
+		System.out.println("Wrong extension!");
+//target
+	}else if(args[2] != "--target")
+	{
+		read(args[1],""); //args[3]
+		Thread.sleep(3000);
+		System.out.println("Wrong/Missing target Command!");
+		Thread.sleep(3000);
+		System.out.println("Do you want to copy? Yes/No");
+		Scanner sc = new Scanner(System.in);
+		String choice = sc.nextLine();
+		sc.close();
+		if (choice.contains("Yes") || choice.contains("yes")) {
+			System.out.println("vmi");
+			System.out.println("Type your filename!");
+//debug!!!
+		//	Scanner input2 = new Scanner (System.in);
+			String target = "C:/Hegga/csacn.txt";// input2.nextLine(); 
+		}else {
+			System.out.println("Thanks");
+			read(args[1],"");
+		}
+//target--wrong extension
+	}else if(!args[3].contains(".txt")) {
+		System.out.println("Wrong extension!");
+		Thread.sleep(3000);
+		System.out.println("You can write it manually, if you want..");
+		Scanner fileinput = new Scanner(System.in);
+		String addtargetpath = fileinput.nextLine();
+		fileinput.close();
+		read(args[1],addtargetpath);
+	}else {
+		read(args[1],args[3]);
+	}
 	
-	if(filename2.length()==0) {
-			File myFile = new File (filename);
+//fix paraméterrel
+	String sourcecommand="--source";
+	String source="C:/Hegga/sample.txt";
+	String targetcommand="--target";
+	String target = "";
+	String argtarget = "C:/Hegga/samefe.tt";
+	
+	if (sourcecommand.length()== 0) {
+		System.out.println("Not a parameter.");
+	}
+//source
+	if(sourcecommand != "--source") {
+		System.out.println("Wrong Command!");
+	}else if(!source.contains(".txt")) {
+		System.out.println("Wrong extension!");
+//target
+	}else if(targetcommand != "--target")
+	{
+		read(source,target);
+		Thread.sleep(3000);
+		System.out.println("Wrong/Missing target Command!");
+		Thread.sleep(3000);
+		System.out.println("Do you want to copy? Yes/No");
+		Scanner sc = new Scanner(System.in);
+		String choice = sc.nextLine();
+		sc.close();
+		if (choice.contains("Yes") || choice.contains("yes")) {
+			System.out.println("vmi");
+			System.out.println("Type your filename!");
+		//	Scanner input2 = new Scanner (System.in);
+			target = "C:/Hegga/csacn.txt";// input2.nextLine(); 
+		}else {
+			System.out.println("Thanks");
+			read(source,target);
+		}
+		read(source,target);
+//target--wrong extension
+	}else if(!argtarget.contains(".txt")) {
+		System.out.println("Wrong extension!");
+		Thread.sleep(3000);
+		System.out.println("You can write it manually, if you want..");
+		Scanner fileinput = new Scanner(System.in);
+		String addtargetpath = fileinput.nextLine();
+		fileinput.close();
+		read(source,addtargetpath);
+	}else {
+		read(source,argtarget);
+	}
+		
+}
+	
+
+public static void read(String source, String target) {
+	
+	if(target.length()==0) {
+			File myFile = new File (source);
 			Scanner myReader;
 			try {
 					myReader = new Scanner (myFile);
@@ -62,14 +126,14 @@ public static void read(String filename, String filename2) {
 				} //ArrayIndexOutOfBound exception
 
 	}else {
-		File myFile = new File (filename);
+		File myFile = new File (source);
 		Scanner myReader;		
 		try {
 			myReader = new Scanner (myFile);
 			while(myReader.hasNextLine()) {
 				String text = myReader.nextLine();									
 				try  { 
-				      FileWriter myWriter = new FileWriter(filename2);
+				      FileWriter myWriter = new FileWriter(target);
 				      myWriter.write(text);
 				      myWriter.close();
 				      System.out.println("Success.");
