95d52204ffc7ead46e0c57fe1fbc836bcba098d5
Sleeepi <34219347+Sleeepi@users.noreply.github.com>
2019-04-05 11:46:20 +0200

    Update ReadTester.java

--- src/test/java/com/thotsoft/heger/readwriterTest/ReadTester.java
+++ src/test/java/com/thotsoft/heger/readwriterTest/ReadTester.java
@@ -8,40 +8,30 @@ import junit.framework.TestCase;
 import junit.runner.Version;
 
 
-class ReadTester extends TestCase {
-	
-	
-/*	@Before
-	public void setUp() {
-		ReaderTest tester = new ReaderTest();
-	} */
+class ReadTester {
+		
 	ReaderTest tester = new ReaderTest();
 	
 	@Test
 	public void testReaderMethod() {
-	   /*
-		String name = "";
-		assertNull("Result", tester.ReaderMethod(name,"" )); */
+		
 		ReaderTest.read("C:/Hegga/sample.txt", "");
 		ReaderTest.read("", "");
 		ReaderTest.read("C:/Hegga/sample1.txt", "C:/Hegga/342342.txt");
 		ReaderTest.read("C:/Hegga/tara1.txt", "");
 		ReaderTest.read("C:/asdas","");
-		System.out.println("JUnit version is: " + Version.id());
-		
+		System.out.println("JUnit version is: " + Version.id());	
     }
-
 	public void testReaderMethodWithEmptyString() {
 	    String name = "";
 	    
-	    ReaderTest.read(name, name);
-	    
-	}
+	    ReaderTest.read(name, name);   
+    }
 	
 	public void testReaderMethodWithNotText() {
 		String szam = "34234234234";
 		ReaderTest.read(szam, szam);
 		ReaderTest.read("653535", "3253234");
-	}
+    }
 }
 
