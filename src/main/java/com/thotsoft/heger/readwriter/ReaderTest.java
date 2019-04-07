1ba1e524ba060f8b1d2c5962111ba6010bc0dec8
Sleeepi <34219347+Sleeepi@users.noreply.github.com>
2019-04-07 22:27:01 +0200

    Add files via upload

--- src/main/java/com/thotsoft/heger/readwriter/ReaderTest.java
+++ src/main/java/com/thotsoft/heger/readwriter/ReaderTest.java
@@ -13,99 +13,53 @@ public class ReaderTest {
 	public static void main (String[] args) throws Exception {
 		
 //args	
-	if (args[0].length()== 0) {
+		if (args[0].length()== 0) {
 		System.out.println("Not a parameter.");
-	}
-//source
-	if(args[0] != "--source") {
-		System.out.println("Wrong Command!");
-	}else if(!args[1].contains(".txt")) {
-		System.out.println("Wrong extension!");
-//target
-	}else if(args[2] != "--target")
-	{
-		read(args[1],""); //args[3]
-		Thread.sleep(3000);
-		System.out.println("Wrong/Missing target Command!");
-		Thread.sleep(3000);
-		System.out.println("Do you want to copy? Yes/No");
-		Scanner sc = new Scanner(System.in);
-		String choice = sc.nextLine();
-		sc.close();
-		if (choice.contains("Yes") || choice.contains("yes")) {
-			System.out.println("vmi");
-			System.out.println("Type your filename!");
-//debug!!!
-		//	Scanner input2 = new Scanner (System.in);
-			String target = "C:/Hegga/csacn.txt";// input2.nextLine(); 
-		}else {
-			System.out.println("Thanks");
-			read(args[1],"");
+	} else {
+		//source command
+		if(args[1] != "--source") {
+			System.out.println("Wrong Command!");
+		} else {
+			if(!args[1].contains(".txt")) {
+				System.out.println("Wrong extension!");
+			} else {
+				if(args[2] != "--target"){
+					read(args[1],"");
+					Thread.sleep(3000);
+					System.out.println("Wrong/Missing target Command!");
+					Thread.sleep(3000);
+					System.out.println("Do you want to copy? Yes/No");
+					Scanner sc = new Scanner(System.in);
+					String choice = sc.nextLine();
+					if (choice.contains("Yes") || choice.contains("yes")) {
+						System.out.println("Type your file location");	
+						String target = sc.nextLine();
+						sc.close();
+					}else {
+						System.out.println("Thanks");
+						read(args[1],"");
+					}
+					read(args[1], args[3]);
+			}else {
+				//target--wrong extension
+				if(!args[3].contains(".txt")) {
+				System.out.println("Wrong extension!");
+				Thread.sleep(3000);
+				System.out.println("You can write it manually, if you want..");
+				Scanner fileinput = new Scanner(System.in);
+				String addtargetpath = fileinput.nextLine();
+				fileinput.close();
+				read(args[1],addtargetpath);
+			}else {
+				read(args[1],args[3]);
+			
+			}
 		}
-//target--wrong extension
-	}else if(!args[3].contains(".txt")) {
-		System.out.println("Wrong extension!");
-		Thread.sleep(3000);
-		System.out.println("You can write it manually, if you want..");
-		Scanner fileinput = new Scanner(System.in);
-		String addtargetpath = fileinput.nextLine();
-		fileinput.close();
-		read(args[1],addtargetpath);
-	}else {
-		read(args[1],args[3]);
 	}
-	
-//fix paraméterrel
-	String sourcecommand="--source";
-	String source="C:/Hegga/sample.txt";
-	String targetcommand="--target";
-	String target = "";
-	String argtarget = "C:/Hegga/samefe.tt";
-	
-	if (sourcecommand.length()== 0) {
-		System.out.println("Not a parameter.");
-	}
-//source
-	if(sourcecommand != "--source") {
-		System.out.println("Wrong Command!");
-	}else if(!source.contains(".txt")) {
-		System.out.println("Wrong extension!");
-//target
-	}else if(targetcommand != "--target")
-	{
-		read(source,target);
-		Thread.sleep(3000);
-		System.out.println("Wrong/Missing target Command!");
-		Thread.sleep(3000);
-		System.out.println("Do you want to copy? Yes/No");
-		Scanner sc = new Scanner(System.in);
-		String choice = sc.nextLine();
-		sc.close();
-		if (choice.contains("Yes") || choice.contains("yes")) {
-			System.out.println("vmi");
-			System.out.println("Type your filename!");
-		//	Scanner input2 = new Scanner (System.in);
-			target = "C:/Hegga/csacn.txt";// input2.nextLine(); 
-		}else {
-			System.out.println("Thanks");
-			read(source,target);
 		}
-		read(source,target);
-//target--wrong extension
-	}else if(!argtarget.contains(".txt")) {
-		System.out.println("Wrong extension!");
-		Thread.sleep(3000);
-		System.out.println("You can write it manually, if you want..");
-		Scanner fileinput = new Scanner(System.in);
-		String addtargetpath = fileinput.nextLine();
-		fileinput.close();
-		read(source,addtargetpath);
-	}else {
-		read(source,argtarget);
-	}
-		
+		}	
 }
-	
+
 
 public static void read(String source, String target) {
 	
