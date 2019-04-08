3723282bdd1dc1c965091d34dd50c02087d5fb10
Sleeepi <34219347+Sleeepi@users.noreply.github.com>
2019-04-08 14:38:38 +0200

    Add files via upload

--- src/main/java/com/thotsoft/heger/readwriter/ReaderTest.java
+++ src/main/java/com/thotsoft/heger/readwriter/ReaderTest.java
@@ -12,17 +12,16 @@ public class ReaderTest {
     public static void main (String[] args) throws Exception {
 //args
         try {
-            if (args[0].length()== 0) {
+            if (args[0].length() == 0) {
             System.out.println("Not a parameter.");
             } else {
-            //source command
-                if(args[1]!= "--source") {
+                if (!args[0].contains("--source")) {
                     System.out.println("Wrong Command!");
                 } else {
-                    if(!args[1].contains(".txt")) {
+                    if (!args[1].contains(".txt")) {
                         System.out.println("Wrong extension!");
                     } else {
-                        if(args[2]!= "--target"){
+                        if (args[2]!= "--target"){
                             read(args[1],"");
                             Thread.sleep(3000);
                             System.out.println("Wrong/Missing target Command!");
@@ -33,6 +32,7 @@ public class ReaderTest {
                                 if (choice.contains("Yes") || choice.contains("yes")) {
                                     System.out.println("Type your file location");
                                     String target = sc.nextLine();
+                                    
                                     sc.close();
                                 } else {
                                     System.out.println("Thanks");
@@ -40,13 +40,13 @@ public class ReaderTest {
                                 }
                             read(args[1], args[3]);
                         } else {
-                            //target--wrong extension
-                            if(!args[3].contains(".txt")) {
+                            if (!args[3].contains(".txt")) {
                                 System.out.println("Wrong extension!");
                                 Thread.sleep(3000);
                                 System.out.println("You can write it manually, if you want..");
                                 Scanner fileinput = new Scanner(System.in);
                                 String addtargetpath = fileinput.nextLine();
+                                
                                 fileinput.close();
                                 read(args[1], addtargetpath);
                             } else {
@@ -58,12 +58,12 @@ public class ReaderTest {
             }
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("You didn't type a parameter.");
-        }
+        } 
 }
 
-    public static void read(String source, String target) {
+    public static void read(String source, String target) throws IOException {
 
-        if(target.length()== 0) {
+        if (target.length() == 0) {
             File myFile = new File (source);
             Scanner myReader;
             try {
@@ -72,30 +72,27 @@ public class ReaderTest {
                     String text = myReader.nextLine();
                     System.out.println(text);
                 }
-            myReader.close();
+                myReader.close();
             } catch (FileNotFoundException e) {
                 System.out.println("File not found.");
-            } //ArrayIndexOutOfBound exception
+            }
         } else {
             File myFile= new File (source);
             Scanner myReader;
             try {
                 myReader = new Scanner (myFile);
-                while(myReader.hasNextLine()) {
-                String text = myReader.nextLine();
-                    try { 
-                        FileWriter myWriter = new FileWriter(target);
-                        myWriter.write(text);
-                        myWriter.close();
-                        System.out.println("Success.");
-                    } catch (IOException e) {
-                        System.out.println("IO-Error");
-                        e.printStackTrace();
-                    }
-                }
+                FileWriter myWriter = new FileWriter(target);
+                while (myReader.hasNextLine()) {
+                    String text = myReader.nextLine();
+                    
+                    myWriter.write(text);
+                    myWriter.write(System.getProperty("line.separator"));
+                    myWriter.flush();
+                } System.out.println("Success.");
+                  myWriter.close();  
             } catch (FileNotFoundException e) {
                 System.out.println("File not found."); 
             }
-        }
+        } 
     }
 }
